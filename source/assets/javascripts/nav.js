6cabf036560ee84a6b8dc42b8edd9521d7d07f5a
Devin Jameson <devin@thoughtbot.com>
2019-09-16 17:34:36 -0400

    Create smooth scroll and navigation tracking functionality

--- source/assets/javascripts/nav.js
+++ source/assets/javascripts/nav.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
 
   $('.header__nav a').on('click', function(e) {
 
+    $(this).removeClass('active');
+
     // Prevent default behavior
 
     e.preventDefault();
@@ -14,10 +16,10 @@ $(document).ready(function(){
     // Animate scroll
     $('html, body').animate(
       {
-        scrollTop: $($(this).attr('href')).offset().top,
+        scrollTop: $($(this).attr('href')).offset().top-50,
       },
-      500,
-      'linear'
+      300,
+      'swing'
     );
 
     // Add hash to URL
@@ -25,10 +27,33 @@ $(document).ready(function(){
 
   });
 
-  
+  var lastID,
+      menuItems = $('.header__nav').find("a"),
+      scrollItems = menuItems.map(function(){
+        var item = $($(this).attr("href"));
+        if (item.length) { return item; }
+      });
+
+  $(window).scroll(function(){
+
+    var fromTop = $(this).scrollTop()+$('.header__nav').outerHeight()+275;
+    
+    var cur = scrollItems.map(function(){
+      if ($(this).offset().top < fromTop)
+        return this;
+    });
+
+    cur = cur[cur.length-1];
+    var id = cur && cur.length ? cur[0].id : "";
+
+    if (lastID !== id) {
+        lastID = id;
+        menuItems
+          .parent().removeClass("active")
+          .end().filter("[href='#"+id+"']").parent().addClass("active");
+    }
+
+  });
 
 
-
-
-  
 });
